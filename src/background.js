8e795c48aaecd3677f5c9a3a9aef122810a9eea0
lguerrero1 <luisguerrero12387@gmail.com>
2021-05-13 23:45:01 -0400

    show error in popup

--- src/background.js
+++ src/background.js
@@ -1,1 +1,17 @@
 import browser from 'webextension-polyfill'
+
+const handleMessage= (request, sender, sendResponse) => {
+    console.log('este es el sender',sender);
+      if (request === 'clima') {
+        fetch('') 
+          .then( respuesta => {
+              return respuesta.json();
+          }) 
+          .then(resultado => {
+            sendResponse(resultado);
+          })
+      }
+    
+  }
+  
+    browser.runtime.onMessage.addListener(handleMessage);
--- src/popup.js
+++ src/popup.js
@@ -1,1 +1,41 @@
 import browser from 'webextension-polyfill'
+const container = document.querySelector('.container');
+const result = document.querySelector('#resultado');
+const form= document.querySelector('#formulario');
+console.log(container)
+window.addEventListener('load', () => {
+    form.addEventListener('submit', searchWeather);
+})
+
+function searchWeather(e) {
+    e.preventDefault();
+    //validating
+    const city = document.querySelector('#ciudad').value;
+    const country = document.querySelector('#pais').value;
+
+    if(city === '' || country === '' ){
+        showError('Ambos campos son obligatorios!');
+    }
+}
+
+function showError(message){
+    console.log(message)
+    const alerta = document.querySelector('.alert-danger');
+
+    if(!alerta){//if there are no alerts then
+            //create alert 
+        const alerta = document.createElement('div');
+        alerta.classList.add('alert', 'alert-danger', 'd-flex', 'align-items-center', 'text-center',"px-1", "py-2", "mx-5", "fw-bold", "row");
+
+        alerta.innerHTML = `
+            <strong class="font-bold">Error!</strong>
+            <span class="block">${message}</span>
+        `
+        container.appendChild(alerta);
+        setTimeout(() => {
+            alerta.remove();
+        }, 2000);
+    }
+    
+    
+}
